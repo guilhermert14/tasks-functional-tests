32a57ec3b25bdba343cef69c9a169532f53e36aa
Guilherme Teixeira <guilhermeteiveira@Guilherme-Yaman.local>
2020-04-22 20:03:31 -0300

    test

---
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -0,0 +1,67 @@
+package br.ce.wcaquino.tasks.functional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TasksTest {
+
+    public WebDriver acessarAplicacao(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8001/tasks");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso(){
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+            driver.findElement(By.id("saveButton")).click();
+
+            String message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+        }finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao(){
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+            driver.findElement(By.id("saveButton")).click();
+
+            String message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Fill the task description", message);
+        }finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemData(){
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+            driver.findElement(By.id("saveButton")).click();
+
+            String message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Fill the due date", message);
+        }finally {
+            driver.quit();
+        }
+    }
+}
